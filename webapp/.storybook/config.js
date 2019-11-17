55e2381c932519e5b641647f2add3cb69dfe604a
Amine <mouhamedaymenhammami@gmail.com>
2019-11-17 18:29:47 +0100

    storybook background addon

--- webapp/.storybook/config.js
+++ webapp/.storybook/config.js
@@ -1,9 +1,13 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../components", true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  backgrounds: [{ name: "default", value: "#f3f5f7", default: true }]
+});
 
 configure(loadStories, module);
