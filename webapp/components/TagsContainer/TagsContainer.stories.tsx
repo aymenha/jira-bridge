e3b62537db070d72c789349e5f44e779f6f15a90
Hamdi Gatri <hamdigatri92@gmail.com>
2020-08-28 11:12:44 +0100

    Create TagsContainer component

---
+++ webapp/components/TagsContainer/TagsContainer.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { host } from "storybook-host";
+import TagsContainer from "./TagsContainer";
+
+const Host = host({
+  align: "center middle",
+  background: true,
+  backdrop: true
+});
+
+export const GeneratedColors = () => (
+  <TagsContainer list={["API", "WebApp", "DB"]} />
+);
+
+export default {
+  title: "TagsContainer",
+  decorators: [Host]
+};
