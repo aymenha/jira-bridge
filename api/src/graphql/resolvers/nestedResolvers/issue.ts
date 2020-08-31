5f881faf3c3afbe805d9339f101446b6ea13ad96
fakhergh <fekhergh@xtech.guru>
2020-08-31 11:56:14 +0100

    [api] JR-04 Add status field to issue.

--- api/src/graphql/resolvers/nestedResolvers/issue.ts
+++ api/src/graphql/resolvers/nestedResolvers/issue.ts
@@ -1,5 +1,6 @@
 export default {
   summary: parent => parent.fields.summary,
   type: parent => parent.fields.issuetype.name,
-  priority: parent => parent.fields.priority.name
+  priority: parent => parent.fields.priority.name,
+  status: parent => parent.fields.status.name
 };
