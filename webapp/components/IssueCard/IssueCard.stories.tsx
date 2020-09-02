b6f636918a760c4fd2323494b29a82a27814ab55
fakhergh <fekhergh@xtech.guru>
2020-09-02 16:32:10 +0100

    [web] JR-08 Add id property to IssueCard stories.

--- webapp/components/IssueCard/IssueCard.stories.tsx
+++ webapp/components/IssueCard/IssueCard.stories.tsx
@@ -13,25 +13,32 @@ const fakeMembersList = [
   { id: 2, name: 'Fakher Gh', picture: 'https://ca.slack-edge.com/T1EHPUWM8-UFWL9UUF3-5cbfb7d112af-512' },
   { id: 3, name: 'Hamdi Gatri', picture: 'https://ca.slack-edge.com/T1EHPUWM8-U7TMLMR8B-8a8f4e4791a7-512' }
 ];
+
+const onClick = () => {
+  console.log('clicked');
+};
+
 export const Default = () => (
-  <IssueCard summary="new task" tags={['webApp', 'Api', 'DB']} assignedTo={fakeMembersList}onClick={() => console.log('clicked')}
-  />
+  <IssueCard id={1} summary="new task" tags={['webApp', 'Api', 'DB']} assignedTo={fakeMembersList} onClick={onClick} />
 );
 
 export const MoreMembers = () => (
   <IssueCard
+    id={1}
     summary="new task"
     tags={['webApp', 'Api', 'DB']}
     assignedTo={[...fakeMembersList, { id: 11, name: 'Mustafa Mokrani' }, { id: 12, name: 'Houssem Jelitti' }]}
-    onClick={() => console.log('clicked')}
+    onClick={onClick}
   />
 );
 
-export const NoTags = () => <IssueCard summary="new task" assignedTo={fakeMembersList} onClick={() => console.log('clicked')} />;
+export const NoTags = () => <IssueCard id={1} summary="new task" assignedTo={fakeMembersList} onClick={onClick} />;
 
-export const NotAssigned = () => <IssueCard summary="new task" onClick={() => console.log('clicked')} />;
+export const NotAssigned = () => <IssueCard id={1} summary="new task" onClick={onClick} />;
 
-export const WithTagsButNotAssigned = () => <IssueCard summary="new task" tags={['webApp', 'Api', 'DB']} onClick={() => console.log('clicked')} />;
+export const WithTagsButNotAssigned = () => (
+  <IssueCard id={1} summary="new task" tags={['webApp', 'Api', 'DB']} onClick={onClick} />
+);
 export default {
   title: 'IssueCard',
   decorators: [Host]
