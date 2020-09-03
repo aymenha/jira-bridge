419bc3015fdd8cd397692d96dfe4f0627ed65bf4
Hamdi Gatri <hamdigatri92@gmail.com>
2020-09-03 14:07:29 +0100

    [web] JR-10 Create IssueCardDetail component

--- webapp/components/IssueCard/IssueCard.tsx
+++ webapp/components/IssueCard/IssueCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-
-import { ButtonBase, Card, Typography, CardContent, makeStyles, Avatar, capitalize } from '@material-ui/core';
+import React, { useState, useCallback } from 'react';
+import { Card, Typography, CardContent, makeStyles, Avatar, capitalize } from '@material-ui/core';
 import { Draggable } from 'react-beautiful-dnd';
+
 import TagsContainer from '../TagsContainer/TagsContainer';
 import MemberAvatarGroup, { Member } from '../MemberAvatarGroup/MemberAvatarGroup';
+import IssueCardDetails from '../IssueCardDetails/IssueCardDetails';
 
 const useStyles = makeStyles({
   title: {
@@ -26,45 +27,63 @@ const useStyles = makeStyles({
     justifyContent: 'flex-start'
   }
 });
-
+interface SubTask {
+  title: string;
+  onwer: Member;
+  isCompleted: boolean;
+}
 export interface IssueCardType {
   id: number;
   summary: string;
   tags?: string[];
   assignedTo?: Member[];
+  subTasks?: SubTask[];
 }
 interface IssueCardProps extends IssueCardType {
   onClick: () => void;
   key?: string;
   index?: number;
 }
-export default ({ summary, tags, assignedTo, onClick, id, index }: IssueCardProps) => {
+
+export default ({ summary, id, index, tags = [], assignedTo = [], subTasks = [] }: IssueCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const classes = useStyles();
+
+  const onClick = useCallback(() => {
+    setIsModalOpen(true);
+  }, [isModalOpen]);
   return (
-    <Draggable draggableId={`draggable-${id}`} index={index} type="ISSUE">
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <Card className={classes.cardContainer}>
-            <CardContent>
-              <Typography className={classes.title} color="textSecondary" gutterBottom variant="h6">
-                {capitalize(summary)}
-              </Typography>
-              <div
-                className={classes.actionsContainer}
-                style={{
-                  justifyContent: tags && tags.length > 0 ? 'space-between' : 'flex-end'
-                }}>
-                {tags && <TagsContainer list={tags} />}
-                {assignedTo && assignedTo.length > 0 ? (
-                  <MemberAvatarGroup list={assignedTo} />
-                ) : (
-                  <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/925px-Unknown_person.jpg" />
-                )}
-              </div>
-            </CardContent>
-          </Card>
-        </div>
-      )}
-    </Draggable>
+    <>
+      <Draggable draggableId={`draggable-${id}`} index={index} type="ISSUE">
+        {provided => (
+          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <Card className={classes.cardContainer} onClick={onClick}>
+              <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom variant="h6">
+                  {capitalize(summary)}
+                </Typography>
+                <div
+                  className={classes.actionsContainer}
+                  style={{
+                    justifyContent: tags && tags.length > 0 ? 'space-between' : 'flex-end'
+                  }}>
+                  {tags && <TagsContainer list={tags} />}
+                  {assignedTo && assignedTo.length > 0 ? (
+                    <MemberAvatarGroup list={assignedTo} />
+                  ) : (
+                    <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/925px-Unknown_person.jpg" />
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        )}
+      </Draggable>
+      <IssueCardDetails
+        isOpen={isModalOpen}
+        handleClose={() => setIsModalOpen(false)}
+        issue={{ summary, id, assignedTo, subTasks }}
+      />
+    </>
   );
 };
---
+++ webapp/components/IssueCardDetails/IssueCardDetails.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Modal, makeStyles, IconButton, Grid, Checkbox, Typography, Button } from '@material-ui/core';
+import { Close as CloseIcon, Add as AddIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  container: {
+    color: 'black',
+    backgroundColor: 'white',
+    margin: 'auto'
+  },
+  settingsContainer: {
+    backgroundColor: '#f0f2f1',
+    color: '#707070',
+    padding: 10
+  },
+  informationContainer: {
+    padding: 10
+  },
+  subTaskContainer: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  listItemsStyle: {}
+});
+
+const Assignees = ({ assignedTo }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <p>Assignees</p>
+      <ul className={classes.listItemsStyle}>
+        <li>
+          <Button variant="contained" color="default" startIcon={<AddIcon />}>
+            Add assignee
+          </Button>
+        </li>
+        {assignedTo &&
+          assignedTo.map(assignee => (
+            <li>
+              <div>
+                <img alt="" />
+              </div>
+              <div>{assignee.name}</div>
+              <div>
+                <IconButton>
+                  <CloseIcon />
+                </IconButton>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+const IssueSubTasks = ({ subTasks }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <p>Checklist</p>
+      <ul>
+        {subTasks.map(task => (
+          <li className={classes.subTaskContainer}>
+            <Checkbox checked={task.isCompleted} /> <Typography> {task.title}</Typography>{' '}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ({ isOpen, issue, handleClose }) => {
+  const classes = useStyles();
+  return (
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="issue-card-details"
+      aria-describedby="issue-card-details-description">
+      <Grid container className={classes.container} spacing={0} xs={6}>
+        <Grid item xs={8} className={classes.informationContainer}>
+          <h2>{issue.summary}</h2>
+          <p>{issue.description}</p>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+              <Typography>Due date</Typography>
+              <Typography>{issue.due_date}</Typography>
+            </div>
+            <div>
+              <p>owner</p>
+            </div>
+          </div>
+          <IssueSubTasks subTasks={issue.subTasks} />
+        </Grid>
+        <Grid item xs={4} className={classes.settingsContainer}>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <IconButton color="primary" aria-label="upload picture" component="span">
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <Assignees assignedTo={issue.assignedTo} />
+        </Grid>
+      </Grid>
+    </Modal>
+  );
+};
